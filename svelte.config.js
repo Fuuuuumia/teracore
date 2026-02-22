fe596d59d09d695bf7fc10bffdb3b98ba8ccfeaa
Fuuuuumia <fumi.r35@gmail.com>
2026-02-22 15:55:31 +0900

    Deployee2

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
-import { mdsvex } from 'mdsvex';
+  import adapter from '@sveltejs/adapter-static';
+  import { mdsvex } from 'mdsvex';
 
-const config = {
-  extensions: ['.svelte', '.md'],
-  preprocess: mdsvex(),
-  kit: {
-    adapter: adapter({
-      pages:'build',
-      assets: 'build',
+  const config = {
+    extensions: ['.svelte', '.md'],
+    preprocess: mdsvex(),
+    kit: {
+      adapter: adapter({
+        pages:'build',
+        assets: 'build',
 
-      fallback: '404.html'
-    }),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        fallback: '404.html'
+      }),
+      paths: {
+        base: process.argv.includes('dev') ? '' : '/teracore'
+      }
     }
-  }
-};
+  };
 
-export default config;
+  export default config;
