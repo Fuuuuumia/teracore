956741a884c0a3021354688207c2d9d4c16905b5
556n0259 <556n.0259@gmail.com>
2026-03-09 13:20:17 +0900

    layout変更

--- src/routes/blog/cat06/(scoped)/+layout.svelte
+++ src/routes/blog/cat06/(scoped)/+layout.svelte
@@ -11,55 +11,55 @@
 <style>
 
   :root{
-    --accent-color: #003160;
-    --font-size-h1: 28em;
-    --font-size-h2: 24em;
-    --font-size-h3: 20em;
-    --font-size-p: 16em;
+    --bg-color: #fdfdfd;
+    --main-color: #003160;
+    --sub-color: #e6edf5;
+    --pre-bg-color: #262626;
+    --pre-code-color: #fdfdfd;
+    --font-size-h1: calc(28rem / 16);
+    --font-size-h2: calc(24rem / 16);
+    --font-size-h3: calc(20rem / 16);
+    --font-size-p: calc(16rem / 16);
   }
   @media (max-width: 500px){
     :root{
-    --font-size-h1:24em;
-    --font-size-h2: 20em;
-    --font-size-h3:18em;
+    --font-size-h1: calc(24rem / 16);
+    --font-size-h2: calc(20rem / 16);
+    --font-size-h3: calc(18rem / 16);
     }
-}
+  }
 
-.simplelog{
-  background: #fdfdfd;
-  overflow: hidden;
-  padding-top: 1rem;
-}
+  .simplelog{
+    background: var(--bg-color);
+    overflow: hidden;
+    padding-top: 1rem;
+  }
   .content {
     max-width: 800px;
     margin: 0 auto;
     padding: 0 24px;
-    font-size: calc(1rem/16);
     line-height: 1.5;
   }
 
   .simplelog :global(h1) {
-    color: var(--accent-color);
+    color: var(--main-color);
     font-size: var(--font-size-h1);
     margin: 1rem 0 1.5rem;
     padding-bottom: 0.5rem;
-    border-bottom: 3px solid var(--accent-color);
+    border-bottom: 2px solid var(--main-color);
   }
-
   .simplelog :global(h2) {
-    color: var(--accent-color);
+    color: var(--main-color);
     font-size: var(--font-size-h2);
     margin: 3rem 0 0;
     padding-left: 0.75rem;
-    border-left: 5px solid var(--accent-color);
+    border-left: 3px solid var(--main-color);
   }
-
   .simplelog :global(h3) {
     font-size: var(--font-size-h3);
     margin: 2rem 0 0;
     color: #222;
   }
-
   .simplelog :global(p){
     font-size: var(--font-size-p);
     margin: 1.5rem 0;
@@ -70,65 +70,60 @@
     margin: 1.5rem 0;
     padding-left: 1.5rem;
   }
-
   .simplelog :global(li) {
-    margin: 0.6rem 0;
+    font-size: var(--font-size-p);
+    margin: 0.5rem 0;
   }
-
   .simplelog :global(li::marker) {
-    color: var(--accent-color);
+    color: var(--main-color);
     font-weight: bold;
   }
 
-
-.simplelog :global(hr) {
-  border: none;
-  height: 3px;
-  background: #e5e7eb;
-  margin: 4rem 0;
-}
-
-
-  .simplelog :global(pre) {
-    background: #0f172a;
-    color: #e2e8f0;
-    padding: 1.2rem;
-    border-radius: 10px;
-    overflow-x: auto;
-    margin: 2rem 0;
-    font-size: 0.9rem;
+  .simplelog :global(hr) {
+    height: 2px;
+    margin: 3rem 0;
+    background: var(--sub-color);
+    border: none;
   }
 
   .simplelog :global(code) {
-    background: #e6edf5;
-    padding: 0.2rem 0.4rem;
-    border-radius: 6px;
-    font-size: 0.9rem;
+    font-size: calc(var(--font-size-p) * 0.9);
+    padding: 0.2rem 0.3rem;
+    background: var(--sub-color);
+    border-radius: 0.25rem;
   }
-
+  .simplelog :global(pre) {
+    font-size: 0.9rem;
+    background: var(--pre-bg-color);
+    color: var(--pre-code-color);
+    padding: 1.2rem;
+    border-radius: 10px;
+    margin: 2rem 0;
+    overflow-x: auto;
+  }
   .simplelog :global(pre code) {
     background: transparent;
     padding: 0;
   }
 
-
   .simplelog :global(blockquote) {
-    border-left: 4px solid var(--accent-color);
-    background: #f4f8fc;
-    padding: 1rem 1.2rem;
+    padding: 0.5rem 1.5rem;
     margin: 2rem 0;
-    color: #333;
+    color: color-mix(in srgb, currentColor 80%, var(--bg-color));;
+    overflow: hidden;
+    background-color: color-mix(in srgb, var(--main-color) 20%, var(--bg-color));
+    border-left: 2px solid var(--main-color);
   }
 
-
   .simplelog :global(img) {
-    max-width: 60%;
+    max-width: calc((100vw - 48px)*0.8);
+    max-height: calc((100vw - 48px)*0.8);
+    width: auto;
     border-radius: 12px;
     margin: 2.5rem auto;
     display: block;
     box-shadow: 0 10px 25px rgba(0, 0, 0, 0.08);
   }
-
 
   .simplelog :global(table) {
     width: 100%;
@@ -137,7 +132,7 @@
   }
 
   .simplelog :global(th) {
-    background: var(--accent-color);
+    background: var(--main-color);
     color: white;
     padding: 0.8rem;
     text-align: left;
@@ -154,7 +149,7 @@
 
 
   .simplelog :global(strong) {
-    color: var(--accent-color);
+    color: var(--main-color);
     font-weight: 700;
   }
 
@@ -164,14 +159,14 @@
 
 
   .simplelog :global(a) {
-    color: var(--accent-color);
+    color: var(--main-color);
     text-decoration: none;
-    border-bottom: 1px solid var(--accent-color);
+    border-bottom: 1px solid var(--main-color);
     transition: 0.2s ease;
   }
 
   .simplelog :global(a:hover) {
-    background: var(--accent-color);
+    background: var(--main-color);
     color: white;
     padding: 0 4px;
   }
