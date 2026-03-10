eb70ed7950e5452d7e70f8092b9670fd179d1617
556n0259 <556n.0259@gmail.com>
2026-03-10 12:54:08 +0900

    articles.ts 変更

--- src/routes/blog/04-aviutlscript/articles.ts
+++ src/routes/blog/04-aviutlscript/articles.ts
@@ -1,13 +1,28 @@
 import type { Article } from "$lib/data";
 
-export const articles: Article[] = [
-    {
-        id: '04-aviutlscript-01',
-        cat: '04-aviutlscript',
-        path: '/blog/04-aviutlscript/01',
-        title: '1.AviUtlスクリプトとは',
-        ath: '04',
-        date: '2026-03-09',
-        index: 1,
-    },
+// 固定値
+const common = {
+  cat: new URL(".", import.meta.url).pathname.split("/").filter(Boolean).at(-1)!,
+  ath: "04",
+};
+
+type InputArticle = Omit<Article, keyof typeof common | "index"> & Partial<Article>;
+
+// 記事リスト
+const input: InputArticle[] = [
+  {
+    id: "04-aviutlscript-01",
+    path: "/blog/04-aviutlscript/01",
+    title: "1.AviUtlスクリプトとは",
+    date: "2026-03-09",
+  },
 ];
+
+// 固定値 + index補完
+export const articles: Article[] =
+  input.map((data, i) => ({
+    index: i + 1,
+    ...common,
+    ...data,
+  }));
+  
