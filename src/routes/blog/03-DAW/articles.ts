91d69d3697936f343a229ac587488d5359dab202
kinako-pro <265059355+kinako-pro@users.noreply.github.com>
2026-03-15 20:13:13 +0900

    記事書いた（超長文）

---
+++ src/routes/blog/03-DAW/articles.ts
@@ -0,0 +1,40 @@
+import type { Article } from "$lib/data";
+
+export const articles: Article[] = [
+    {
+        id: '03-DAW-01',
+        cat: '03-DAW',
+        path: '/blog/03-DAW/file6-1',
+        title: '第１回 講義ガイダンス（内容の前に）',
+        ath: '03',
+        date: '2026-03-15',
+        index: 1
+    },
+    {
+        id: '03-DAW-02',
+        cat: '03-DAW',
+        path: '/blog/03-DAW/file6-2',
+        title: '第２回 DAWを導入しよう！',
+        ath: '03',
+        date: '2026-03-15',
+        index: 2
+    },
+     {
+        id: '03-DAW-03',
+        cat: '03-DAW',
+        path: '/blog/03-DAW/file6-3',
+        title: '第３回 DAWの導入・超基本操作（Studio One 6編）',
+        ath: '03',
+        date: '2026-03-16',
+        index: 2
+    },
+    {
+        id: '03-DAW-04',
+        cat: '03-DAW',
+        path: '/blog/03-DAW/file6-4',
+        title: '第４回 DAWの導入・超基本操作（LUNA編）',
+        ath: '03',
+        date: '2026-03-16',
+        index: 2
+    }
+];
